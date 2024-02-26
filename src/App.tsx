c57857c7af8478cd3c3003f861d3aec5e67f9dc8
joaorocha <joaoeduardorrocha@gmail.com>
2024-02-26 16:53:06 -0300

    feat: change name file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css'
+import './App.scss'
+import ButtonLogin from './Components/ButtonLogin'
 
 function App() {
   return (
     <>
-        
+      <ButtonLogin />
     </>
   )
 }
