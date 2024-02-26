37713be4bbc3876d9933fe8082dee4439630517b
joaorocha <joaoeduardorrocha@gmail.com>
2024-02-26 17:14:04 -0300

    feat: create var colors

---
+++ src/Components/ButtonLogin/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./index.scss";
+
+const ButtonLogin: React.FC = () => {
+
+  return (
+    <button className='btn-login-container'>
+      Criar Conta
+    </button>
+  );
+};
+
+export default ButtonLogin;
