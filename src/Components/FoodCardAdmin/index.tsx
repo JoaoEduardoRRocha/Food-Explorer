f96024babbfa0631b28f48cfa68316f9c5e3d380
joaorocha <joaoeduardorrocha@gmail.com>
2024-02-28 16:14:59 -0300

    feat: new food-card-admin

---
+++ src/Components/FoodCardAdmin/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import "./index.scss";
+import { Food } from '../../Models/food';
+import { PiPencilSimple } from "react-icons/pi";
+
+interface FoodCardAdminProps {
+  food: Food;
+}
+
+const FoodCardAdmin: React.FC<FoodCardAdminProps> = ({ food }) => {
+  return (
+    <article className='article-content'>
+      <div className='article-content__card-container' key={food._id}>
+        <div className='article-content__card-container__img-food'>
+          <img
+            className='article-content__card-container__img-food-size'
+            src={food.image}
+            alt=""
+          />
+        </div>
+        <div className='article-content__card-container__name-food'>
+          {food.name}
+        </div>
+        <div className='article-content__card-container__description-food'>
+          {food.description}
+        </div>
+        <div className='article-content__card-container__price-food'>
+          R$ {food.price.toFixed(2)}
+        </div>
+        <div className='article-content__card-container__pencil-icon'>
+          <PiPencilSimple
+            size={25}
+          />
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default FoodCardAdmin;
