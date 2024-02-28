34c554cff2c2993526f158767e4b3221a9c5b594
joaorocha <joaoeduardorrocha@gmail.com>
2024-02-28 16:13:39 -0300

    feat: add routes signin, singup, forgotpassword

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.scss'
-import AddPlateAdmin from './PagesAdmin/AddPlateAdmin'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import ForgotPassword from './Pages/ForgotPassword'
+import Home from './Pages/Home'
+import HomeAdmin from './PagesAdmin/HomeAdmin'
+import FoodDetail from './Pages/FoodDetail'
 
 function App() {
   return (
     <>
-      <AddPlateAdmin />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/home-admin" element={<HomeAdmin />} />
+          <Route path="/food-detail" element={<FoodDetail />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
