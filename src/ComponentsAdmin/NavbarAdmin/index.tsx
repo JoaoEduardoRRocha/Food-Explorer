b45d1c2d7147676ad0bb57c8c5a206ee9cad1ede
joaorocha <joaoeduardorrocha@gmail.com>
2024-03-01 10:43:26 -0300

    feat: add nav admin, article-card admin, food-card admin

---
+++ src/ComponentsAdmin/NavbarAdmin/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import "./index.scss";
+
+import { IoIosSearch } from "react-icons/io";
+import { MdLogout } from "react-icons/md";
+import { Link } from 'react-router-dom';
+
+
+const Navbar: React.FC = () => {
+
+  return (
+    <nav className='navbar-container'>
+      <div className='navbar-content'>
+        <div className='navbar-container__logo-explorer'>
+          <img
+            className='navbar-container__logo-explorer__icon-size'
+            src="https://64.media.tumblr.com/dd33e1531a07320c65d37b66c7f49acd/b96640fe255e9488-7a/s100x200/49505833860e4aee0c5e40ca97f8f933d420cc28.pnj"
+            alt="" />
+          <div>
+            <p className='navbar-container__logo-explorer-text'>food explorer</p>
+            <p className='navbar-container__logo-explorer-admin'>admin</p>
+          </div>
+        </div>
+
+        <div className='navbar-container__input-search'>
+          <IoIosSearch
+            size={24}
+          />
+          <input
+            className='navbar-container__input-search__input-bg'
+            type="search"
+            placeholder='Busque por um prato ou ingrediente'
+          />
+        </div>
+
+        <Link
+          className="link-style"
+          to='/add-plate-admin'
+        >
+          <div className='navbar-container__orders'>
+            <p>Novo prato</p>
+          </div>
+        </Link>
+      <div
+        className='navbar-container__logout__icon-size'
+      >
+        <MdLogout
+          size={32}
+        />
+      </div>
+    </div>
+    </nav >
+  );
+};
+
+export default Navbar;
