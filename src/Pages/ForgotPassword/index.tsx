0332cb4e1290f05ed17c7614612583d18b5190de
joaorocha <joaoeduardorrocha@gmail.com>
2024-03-13 11:07:16 -0300

    feat: add check authentication

--- src/Pages/ForgotPassword/index.tsx
+++ src/Pages/ForgotPassword/index.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom'
 import "./index.scss";
 import ButtonLogin from "../../Components/ButtonLogin"
 import InputForm from "../../Components/InputForm"
 import LabelForm from "../../Components/LabelForm"
 import { Link } from 'react-router-dom';
 
-const ForgotPassword: React.FC = () => {
+interface ForgotPassword {
+  email: string
+}
+
+const ForgotPassword = ({ isAuthenticated } : {isAuthenticated: boolean}) => {
+  const navigate = useNavigate()
+  const [forgotPassword, setForgotPassword] = React.useState<ForgotPassword>({
+    email: '',
+  });
+
+  useEffect(() => {
+    if(isAuthenticated) {
+      navigate('/home')
+    } else {
+      console.log('Erro na autenticação!')
+    }
+  })
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForgotPassword((prevForgotPassword) => ({
+      ...prevForgotPassword,
+      [e.target.name]: e.target.value
+    }));
+  }
+
+  const handleSubmit = () => {
+    axios.post('http://localhost:3000/api/auth/forgot-password', forgotPassword)
+      .then(() => {
+        navigate('/home')
+      })
+      .catch((err) => console.log(err))
+  }
 
   return (
     <main className='forgot-password-background'>
@@ -31,10 +64,14 @@ const ForgotPassword: React.FC = () => {
             backgroundColor='#001119'
             textColor='#7C7C8A'
             border="1px solid white"
+            name='email'
+            value={forgotPassword.email}
+            onChange={(e) => handleInputChange(e)}
           />
 
           <ButtonLogin
             buttonText="Resetar"
+            onClick={() => handleSubmit()}
           />
 
           <Link
