4729f2884dbd111f55dbc1ab2ad5ca7ee14b78ac
joaorocha <joaoeduardorrocha@gmail.com>
2024-03-13 14:45:56 -0300

    feat: add input file component

---
+++ src/Components/InputFile/index.tsx
@@ -0,0 +1,15 @@
+import "./index.scss";
+
+const InputFile = () => {
+  return (
+    <input
+    className='add-plate-container__content__upload-file'
+    id="updload-file"
+    type='file'
+    name='image'
+  />
+  );
+};
+
+export default InputFile;
+
