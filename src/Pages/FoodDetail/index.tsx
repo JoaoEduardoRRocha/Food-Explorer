e94c3beececbf372774703c49e7b1f06c5c1e6bf
joaorocha <joaoeduardorrocha@gmail.com>
2024-03-13 11:06:42 -0300

    feat: add verification authenticated and show navbar authenticated or navbar comun

--- src/Pages/FoodDetail/index.tsx
+++ src/Pages/FoodDetail/index.tsx
@@ -1,62 +1,76 @@
-import React from 'react'
+import { useEffect } from 'react'
 import "./index.scss";
 import Navbar from '../../Components/Navbar';
 import Footer from '../../Components/Footer';
 
 import { IoIosArrowBack } from "react-icons/io";
 import { FaMinus, FaPlus } from "react-icons/fa6";
+import { Link, useNavigate } from 'react-router-dom';
+import NavbarAuthenticated from '../../Components/NavbarAuthenticated';
 
-const FoodDetail: React.FC = () => {
+const FoodDetail = ({ isAuthenticated }: { isAuthenticated: boolean }) => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/home')
+    }
+  })
+
 
   return (
     <article className='food-detail-container'>
-      <Navbar />
+      {isAuthenticated ? <NavbarAuthenticated /> : <Navbar />}
       <div className='food-detail-container__content-max-width'>
-        <div className='food-detail-container__content__btn-back'>
-          <IoIosArrowBack
-            size={25}
-          />
-          <div className='food-detail-container__content__btn-back-text'>
-            voltar
-          </div>
-        </div>
-        <div className='food-detail-container__content'>
-          <div className='food-detail-container__content__plate'>
-            <div>
-              <img
-                className='food-detail-container__content__plate-img'
-                src="https://64.media.tumblr.com/8d7ea49ff7eac66034277360b62d8bc3/d37ed12966cfc5da-e5/s1280x1920/a3b9ffac74bb840c551532a8c89003073166b3c2.pnj"
-                alt=""
-              />
-            </div>
 
-            <div className='food-detail-container__content__plate-name-description'>
-              <p className='food-detail-container__content__plate-name'>Salada Ravanello</p>
-              <p className='food-detail-container__content__plate-description'>Rabanetes, folhas verdes e molho agridoce salpicados com gergelim. O pão naan dá um toque especial.</p>
-              <div className='food-detail-container__content__plate-ingredients'>
-                alface
-              </div>
-
-              <div className='food-detail-container__content__plate-quantify-container'>
-                <div className='food-detail-container__content__plate-quantify-minus'>
-                  <FaMinus
-                    size={24}
-                  />
-                </div>
-                <div className='food-detail-container__content__plate-quantify'>01</div>
-                <div className='food-detail-container__content__plate-quantiy-plus'>
-                  <FaPlus
-                    size={24}
-                  />
-                </div>
-                <button className='food-detail-container__content__plate-quantify-btn-include'>incluir ∙ R$ 25,00</button>
-              </div>
+        <Link
+          className='link-style'
+          to="/home">
+          <div className='food-detail-container__content__btn-back'>
+            <IoIosArrowBack
+              size={25}
+            />
+            <div className='food-detail-container__content__btn-back-text'>
+              voltar
             </div>
           </div>
+        </Link>
+      <div className='food-detail-container__content'>
+        <div className='food-detail-container__content__plate'>
+          <div>
+            <img
+              className='food-detail-container__content__plate-img'
+              src="https://64.media.tumblr.com/8d7ea49ff7eac66034277360b62d8bc3/d37ed12966cfc5da-e5/s1280x1920/a3b9ffac74bb840c551532a8c89003073166b3c2.pnj"
+              alt=""
+            />
+          </div>
+
+          <div className='food-detail-container__content__plate-name-description'>
+            <p className='food-detail-container__content__plate-name'>Salada Ravanello</p>
+            <p className='food-detail-container__content__plate-description'>Rabanetes, folhas verdes e molho agridoce salpicados com gergelim. O pão naan dá um toque especial.</p>
+            <div className='food-detail-container__content__plate-ingredients'>
+              alface
+            </div>
+
+            <div className='food-detail-container__content__plate-quantify-container'>
+              <div className='food-detail-container__content__plate-quantify-minus'>
+                <FaMinus
+                  size={24}
+                />
+              </div>
+              <div className='food-detail-container__content__plate-quantify'>01</div>
+              <div className='food-detail-container__content__plate-quantiy-plus'>
+                <FaPlus
+                  size={24}
+                />
+              </div>
+              <button className='food-detail-container__content__plate-quantify-btn-include'>incluir ∙ R$ 25,00</button>
+            </div>
+          </div>
         </div>
-        <Footer />
       </div>
-    </article>
+      <Footer />
+    </div>
+    </article >
   );
 };
 
