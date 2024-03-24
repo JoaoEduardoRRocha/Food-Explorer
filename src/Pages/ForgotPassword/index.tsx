1a51f7382cadf4d9dc7d64e9bd139e964a8ca2de
joaorocha <joaoeduardorrocha@gmail.com>
2024-03-24 20:00:06 -0300

    feat: add new path and link

--- src/Pages/ForgotPassword/index.tsx
+++ src/Pages/ForgotPassword/index.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react'
-import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { PATH } from '../../app/path';
 import { useNavigate } from 'react-router-dom'
+
+import axios from 'axios';
 import "./index.scss";
 import ButtonLogin from "../../Components/ButtonLogin"
 import InputForm from "../../Components/InputForm"
 import LabelForm from "../../Components/LabelForm"
-import { Link } from 'react-router-dom';
 
 interface ForgotPassword {
   email: string
@@ -33,7 +35,7 @@ const ForgotPassword = ({ isAuthenticated } : {isAuthenticated: boolean}) => {
   }
 
   const handleSubmit = () => {
-    axios.post('http://localhost:3000/api/auth/forgot-password', forgotPassword)
+    axios.post(`${PATH}/api/auth/forgot-password`, forgotPassword)
       .then(() => {
         navigate('/home')
       })
@@ -43,15 +45,6 @@ const ForgotPassword = ({ isAuthenticated } : {isAuthenticated: boolean}) => {
   return (
     <main className='forgot-password-background'>
       <div className='forgot-password-container'>
-        <div className='forgot-password-container__food-explorer'>
-          <img
-            className='forgot-password-container__food-explorer__icon'
-            src="https://64.media.tumblr.com/dd33e1531a07320c65d37b66c7f49acd/4792f286ff3dcb78-15/s100x200/88b955bac07de7ef8685950c208a793bda1afc41.pnj"
-            alt=""
-          />
-          <p className='forgot-password-container__food-explorer__name-logo'>food explorer</p>
-        </div>
-
         <div className='forgot-password-container__form-container'>
           <p className='forgot-password-container__form-container__card-title'>Recuperar Senha</p>
 
