cd1bbfce31ea1e7992816a0ad400e29805744e5f
joaorocha <joaoeduardorrocha@gmail.com>
2024-03-24 19:51:47 -0300

    feat: add condition for navbar and new styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { useEffect, useState } from 'react';
+import { PATH } from './app/path';
+import { getUser } from './app/auth-helper';
+
 import './App.scss'
+import axios from 'axios'
 import Loading from './Components/Loading'
 import SignIn from './Pages/SignIn'
 import SignUp from './Pages/SignUp'
@@ -12,22 +16,15 @@ import FoodDetail from './Pages/FoodDetail'
 import FoodDetailAdmin from './PagesAdmin/FoodDetailAdmin'
 import AddPlateAdmin from './PagesAdmin/AddPlateAdmin'
 import EditPlateAdmin from './PagesAdmin/EditPlateAdmin'
-import { getUser } from './app/auth-helper';
-import { User } from './Models/user';
+import Navbar from './Components/Navbar'
 
 function App() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [isAdmin, setIsAdmin] = useState<boolean>(false);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-  const [user, setUser] = useState<User>({
-    _id: '0',
-    name: '',
-    email: '',
-    role: '',
-    createdAt: '',
-  });
+  const [foods, setFoods] = useState([])
 
- const checkAuth = async () => {
+  const checkAuth = async () => {
     const currentUser = await getUser()
     if (currentUser) {
       setIsAuthenticated(true)
@@ -35,7 +32,12 @@ function App() {
     if (currentUser.role === 'admin') {
       setIsAdmin(true)
     }
-    setUser(currentUser)
+  }
+
+  const checkFoods = async () => {
+    await axios.get(`${PATH}/api/foods`).then((response) => {
+      setFoods(response.data)
+    })
   }
 
   useEffect(() => {
@@ -43,24 +45,28 @@ function App() {
       setIsLoading(false)
     }, 1000);
     checkAuth()
+    checkFoods()
   }, [isLoading])
 
   return (
     <>
       <BrowserRouter>
         {isLoading ? <Loading /> :
+        <div className='background'>
+          <Navbar foods={foods} isAdmin={isAdmin} isAuthenticated={isAuthenticated} />
           <Routes>
             <Route path="/login" element={<SignIn isAuthenticated={isAuthenticated} isAdmin={isAdmin} />} />
             <Route path="/sign-up" element={<SignUp isAuthenticated={isAuthenticated} />} />
             <Route path="/forgot-password" element={<ForgotPassword isAuthenticated={isAuthenticated} />} />
             <Route path="/change-password" element={<ChangePassword isAuthenticated={isAuthenticated} />} />
-            <Route path="/home" element={<Home isAuthenticated={isAuthenticated} />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/home-admin" element={<HomeAdmin isAdmin={isAdmin} />} />
-            <Route path="/food-detail" element={<FoodDetail isAuthenticated={isAuthenticated} />} />
-            <Route path="/food-detail-admin" element={<FoodDetailAdmin isAdmin={isAdmin}/>} />
-            <Route path="/add-plate-admin" element={<AddPlateAdmin isAdmin={isAdmin}/>} />
-            <Route path="/edit-plate-admin" element={<EditPlateAdmin isAdmin={isAdmin}/>} />
+            <Route path="/food-detail" element={<FoodDetail />} />
+            <Route path="/food-detail-admin" element={<FoodDetailAdmin isAdmin={isAdmin} />} />
+            <Route path="/add-plate-admin" element={<AddPlateAdmin isAdmin={isAdmin} />} />
+            <Route path="/edit-plate-admin" element={<EditPlateAdmin isAdmin={isAdmin} />} />
           </Routes>
+        </div>
         }
       </BrowserRouter>
     </>
