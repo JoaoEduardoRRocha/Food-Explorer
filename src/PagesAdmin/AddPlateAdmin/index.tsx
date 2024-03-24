9bc0ee1d35f386ad7c030e83f6f223986b4ee60c
joaorocha <joaoeduardorrocha@gmail.com>
2024-03-24 20:01:34 -0300

    feat: add operations of inputs add plate

--- src/PagesAdmin/AddPlateAdmin/index.tsx
+++ src/PagesAdmin/AddPlateAdmin/index.tsx
@@ -1,10 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import "./index.scss";
-import NavbarAdmin from '../../ComponentsAdmin/NavbarAdmin';
 import Footer from '../../Components/Footer';
-// import SelectComponent from '../../Components/SelectComponent';
-
 import { IoIosArrowBack, IoMdClose } from "react-icons/io";
 import { FiUpload } from "react-icons/fi";
 import { Link } from 'react-router-dom';
@@ -12,6 +9,7 @@ import { Food } from '../../Models/food'
 import { useNavigate } from 'react-router-dom'
 import SelectComponent from '../../Components/SelectComponent';
 import InputFile from '../../Components/InputFile';
+import { PATH } from '../../app/path';
 
 const AddPlateAdmin = ({ isAdmin }: { isAdmin: boolean }) => {
   const navigate = useNavigate()
@@ -22,32 +20,64 @@ const AddPlateAdmin = ({ isAdmin }: { isAdmin: boolean }) => {
     }
   })
 
-  const [food, setFoods] = React.useState<Food>({
+  const [food, setFood] = useState<Food>({
     _id: 0,
     name: '',
     description: '',
     price: 0,
     image: '',
-    type: '',
-    ingredients: ''
+    type: 'Refeição',
+    ingredients: []
   });
 
+  const [currentIngredient, setCurrentIngredient] = useState('')
+
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFoods((prevFood) => ({
-      ...prevFood,
-      [e.target.name]: e.target.value
-    }));
+    console.log(e.target.id)
+    setFood({ ...food, [e.target.id]: e.target.value });
+  }
+
+  const handleIngredientInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentIngredient(e.target.value)
   }
 
   const handleTextAreaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setFoods((prevFood) => ({
+    setFood({ ...food, [e.target.id]: e.target.value });
+  }
+
+  const handleCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFood({ ...food, type: e.target.value });
+  }
+
+  const handleSubmitIngredients = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const currentFood = food;
+    currentFood.ingredients.push(currentIngredient);
+    setFood(currentFood);
+    setCurrentIngredient('')
+  }
+
+  const handleDeleteIngredient = (index: number) => {
+    const ingredients = food.ingredients;
+    ingredients.splice(index, 1);
+    setFood((prevFood) => ({
       ...prevFood,
-      [e.target.name]: e.target.value
+      ingredients: ingredients
     }));
   }
 
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        console.log(reader.result);
+      }
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  }
+
   const handleSubmit = () => {
-    axios.post('http://localhost:3000/api/foods/', food)
+    axios.post(`${PATH}/api/foods/`, food)
       .then(() => {
         navigate('/home-admin')
       })
@@ -56,7 +86,6 @@ const AddPlateAdmin = ({ isAdmin }: { isAdmin: boolean }) => {
 
   return (
     <main className='add-plate-container'>
-      <NavbarAdmin />
       <div className='add-plate-container__content-max-width'>
         <div className='add-plate-container__content__btn-back'>
           <Link
@@ -81,13 +110,13 @@ const AddPlateAdmin = ({ isAdmin }: { isAdmin: boolean }) => {
             <div>
               <div>
                 <label className='add-plate-container__content__label'>Imagem do prato</label>
-                <label className='add-plate-container__content__btn-upload-img' htmlFor="updload-file">
+                <label className='add-plate-container__content__btn-upload-img' htmlFor="upload-file">
                   <FiUpload
                     size={24}
                   />
                   Selecionar imagem
                 </label>
-                <InputFile />
+                <InputFile onChange={(e) => handleImageChange(e)} />
               </div>
             </div>
 
@@ -100,10 +129,11 @@ const AddPlateAdmin = ({ isAdmin }: { isAdmin: boolean }) => {
                 </label>
               </div>
               <input
+                id='name'
                 className='add-plate-container__content__field-name'
                 type='text'
                 placeholder='Ex: Salada Ceasar'
-                value={food?.name}
+                value={food.name}
                 name='name'
                 onChange={(e) => handleInputChange(e)}
               />
@@ -116,7 +146,10 @@ const AddPlateAdmin = ({ isAdmin }: { isAdmin: boolean }) => {
                 >
                   Categoria
                 </label>
-                <SelectComponent />
+                <SelectComponent
+                  value={food.type}
+                  onChange={(e) => handleCategoryChange(e)}
+                />
               </div>
             </div>
           </div>
@@ -131,20 +164,27 @@ const AddPlateAdmin = ({ isAdmin }: { isAdmin: boolean }) => {
                 </label>
               </div>
               <div className="add-plate-container__content__ingredients" >
-                <div
-                  className="add-plate-container__content__new-ingredients"
-                >
-                  Pão Naan
-                  <IoMdClose size={15} />
-                </div>
-                <input
-                  className='add-plate-container__content__ingredients-btn'
-                  type='text'
-                  placeholder='Adicionar'
-                  value={food?.ingredients}
-                  name='ingredients'
-                  onChange={(e) => handleInputChange(e)}
-                />
+                {food?.ingredients.map((ingredient, index) => (
+                  <div
+                    key={index}
+                    className="add-plate-container__content__new-ingredients"
+                  >
+                    {ingredient}
+                    <IoMdClose size={15} onClick={() => handleDeleteIngredient(index)} />
+                  </div>
+                ))}
+                <form onSubmit={(e) => {
+                  handleSubmitIngredients(e)
+                }}>
+                  <input
+                    className='add-plate-container__content__ingredients-btn'
+                    type='text'
+                    placeholder='Adicionar'
+                    value={currentIngredient}
+                    name='currentIngredient'
+                    onChange={(e) => handleIngredientInputChange(e)}
+                  />
+                </form>
               </div>
             </div>
 
@@ -157,6 +197,7 @@ const AddPlateAdmin = ({ isAdmin }: { isAdmin: boolean }) => {
                 </label>
               </div>
               <input
+                id='price'
                 className="add-plate-container__content__price"
                 type="text"
                 placeholder='R$ 00,00'
@@ -176,6 +217,7 @@ const AddPlateAdmin = ({ isAdmin }: { isAdmin: boolean }) => {
               </label>
             </div>
             <textarea
+              id='description'
               className="add-plate-container__content__description"
               placeholder='Fale brevemente sobre o prato, seus ingredientes e composição'
               name='description'
@@ -192,7 +234,6 @@ const AddPlateAdmin = ({ isAdmin }: { isAdmin: boolean }) => {
             Salvar alterações
           </button>
         </div>
-
       </div>
       <Footer />
     </main>
